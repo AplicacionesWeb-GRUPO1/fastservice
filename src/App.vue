fe001fb1d3591824ff5edfa5129829ca7fca3496
Fiorella <U202120418@upc.edu.pe>
2023-09-28 17:56:15 -0500

    Fix: bugs

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,15 @@
 import Home from "@/views/HomeView.vue";
 import SideMenu from "@/expert/pages/aside.component.vue"
 import HomeContent from "@/expert/pages/home.content.component.vue"
-import MyClient from "@/expert/pages/myClient.component.vue"
 export default {
   name: "app",
   components: {
     Home,
     SideMenu,
     HomeContent,
-    MyClient,
     'home-component': Home,
     'side-component': SideMenu,
     'home-content':HomeContent,
-    'myclient-component': MyClient,
   },
   data() {
   },
@@ -23,6 +20,5 @@ export default {
 <template>
 <home-component></home-component>
   <SideMenu></SideMenu>
-  <myclient-component></myclient-component>
+  <home-content></home-content>
 </template>
-<style scoped>
