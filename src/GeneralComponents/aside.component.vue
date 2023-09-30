d7964620e41e9f9b5f9ada847738c47f708841bc
Enzo Trujillo <112559987+Rimbow002@users.noreply.github.com>
2023-09-30 11:20:33 -0500

    Update aside.component.vue

--- src/GeneralComponents/aside.component.vue
+++ src/GeneralComponents/aside.component.vue
@@ -1,83 +1,106 @@
-<template>
-  <div class="side-menu">
-    <div class="profile-section">
-      <img src="https://gogeticon.net/files/1925428/fa0cbc2764f70113bf2fad3905933545.png" alt="Profile Image" class="profile-image"/>
-      <div class="profile-info">
-        <div class="profile-name">John Murphy</div>
-        <div class="profile-phone"><i class="pi pi-phone"></i>123-456-7890</div>
+  <template>
+    <div class="side-menu">
+      <div class="profile-section">
+        <img src="https://gogeticon.net/files/1925428/fa0cbc2764f70113bf2fad3905933545.png" alt="Profile Image" class="profile-image"/>
+        <div class="profile-info">
+          <div class="profile-name">{{name}}</div>
+          <div class="profile-phone"><i class="pi pi-phone"></i>123</div>
+        </div>
       </div>
+      <ul class="menu-list">
+        <li><i class="pi pi-home"></i>Home</li>
+        <li><i class="pi pi-users"></i>Mis Clientes</li>
+        <li><i class="pi pi-upload"></i>Mis Publicaciones</li>
+        <li><i class="pi pi-wallet"></i>Mis Pagos</li>
+        <li><i class="pi pi-comments"></i>Chats</li>
+        <li><i class="pi pi-wrench"></i>Herramientas</li>
+        <li><i class="pi pi-cog"></i>Configuracion</li>
+        <li><i class="pi pi-sign-out"></i>Logout</li>
+      </ul>
     </div>
-    <ul class="menu-list">
-      <li><i class="pi pi-home"></i>Home</li>
-      <li><i class="pi pi-users"></i>Mis Clientes</li>
-      <li><i class="pi pi-upload"></i>Mis Publicaciones</li>
-      <li><i class="pi pi-wallet"></i>Mis Pagos</li>
-      <li><i class="pi pi-comments"></i>Chats</li>
-      <li><i class="pi pi-wrench"></i>Herramientas</li>
-      <li><i class="pi pi-cog"></i>Configuracion</li>
-      <li><i class="pi pi-sign-out"></i>Logout</li>
-    </ul>
-  </div>
-</template>
+  </template>
 
-<script>
-export default {
-  name: 'SideMenu'
+  <script>
+  import {UserApiService} from "@/services/user-api.service";
 
-}
-</script>
+  export default {
+    name: 'SideMenu',
+    data() {
+      return {
+        user_info: [],
+        newsApi: new UserApiService(),
+        name: String,
+        cellphone: String
+      };
+    },
+    created() {
+      this.getSource();
+    },
+    methods: {
+      getSource() {
+        this.newsApi.getSources().then((response) => {
+          this.user_info = response.data.user_info;
+          console.log(response.data);
+          this.name= this.user_info.user.name;
+          this.cellphone= this.user_info.user.number;
 
-<style scoped>
-.side-menu {
-  width: 250px;
-  background: linear-gradient(to right, #9b59b6,#3498db);
-  color: #fff;
-  padding: 20px;
-  height: 100%;
-  position: fixed;
-}
+        });
+      },
+    },
+  }
+  </script>
 
-.profile-section {
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-}
+  <style scoped>
+  .side-menu {
+    width: 250px;
+    background: linear-gradient(to right, #9b59b6,#3498db);
+    color: #fff;
+    padding: 20px;
+    height: 100%;
+    position: fixed;
+  }
 
-.profile-image {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  margin-right: 10px;
-}
+  .profile-section {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+  }
 
-.profile-info {
-  display: flex;
-  flex-direction: column;
-}
+  .profile-image {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
 
-.profile-name {
-  font-weight: bold;
-  font-size: 16px;
-  padding-bottom: 5px;
-}
+  .profile-info {
+    display: flex;
+    flex-direction: column;
+  }
 
-.menu-list {
-  list-style-type: none;
-  padding: 10px;
-}
+  .profile-name {
+    font-weight: bold;
+    font-size: 16px;
+    padding-bottom: 5px;
+  }
 
-.menu-list li {
-  margin: 10px 0;
-  cursor: pointer;
-  transition: color 0.5s;
-}
+  .menu-list {
+    list-style-type: none;
+    padding: 10px;
+  }
 
-.menu-list li i {
-  margin-right: 10px;
-  margin-bottom: 20px;
-}
+  .menu-list li {
+    margin: 10px 0;
+    cursor: pointer;
+    transition: color 0.5s;
+  }
 
-.menu-list li:hover {
-  color: #000000;
-}
-</style>
+  .menu-list li i {
+    margin-right: 10px;
+    margin-bottom: 20px;
+  }
+
+  .menu-list li:hover {
+    color: #000000;
+  }
+  </style>
