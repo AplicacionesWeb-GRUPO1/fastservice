5f8191f6dd933860309a07c852ce2ac2ec1a2963
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-10-21 15:40:46 -0500

    fixÃ: routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import PageShowAppointmentsContent from "@/clients/ExpertsAppointmentsScreen/pageShowAppointments-conten.component.vue";
-import UsersProfileCardContent from "@/GeneralComponents/usersProfileCard-content.component.vue";
 import GridProfilesContent from "@/GeneralComponents/gridProfiles-content.component.vue";
 import PageFavoritesExperts from "@/clients/FavoritesExpertsPage/pageFavoritesExperts.vue";
 import ClientProfilePage from "@/clients/ClientProfilePage/clientProfile-page.component.vue";
@@ -9,18 +8,6 @@ import HomeContent from "@/expert/pages/home.content.component.vue";
 import Login from "@/loginPage/Login.vue";
 
 
-
-//routes
-/*
-      <li><i class="pi pi-home"></i>Home</li>
-      <li><i class="pi pi-users"></i>Mis Clientes</li>
-      <li><i class="pi pi-upload"></i>Mis Publicaciones</li>
-      <li><i class="pi pi-wallet"></i>Mis Pagos</li>
-      <li><i class="pi pi-comments"></i>Chats</li>
-      <li><i class="pi pi-wrench"></i>Herramientas</li>
-      <li><i class="pi pi-cog"></i>Configuracion</li>
-      <li><i class="pi pi-sign-out"></i>Logout</li>
- */
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
