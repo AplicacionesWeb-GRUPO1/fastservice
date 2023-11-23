910b6be0c3a34f636ced12ca85795ba331ba4b89
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-11-23 00:21:32 -0500

    updeatida

--- src/services/BaseService-api.service.js
+++ src/services/BaseService-api.service.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export class BaseService {
     constructor() {
         this.http = axios.create({
-            baseURL: "https://fastserviceapi20231103222242.azurewebsites.net/api/v1" // URL base de la API
+            baseURL: "https://turincon-fast-service.azurewebsites.net/api/v1" // URL base de la API
         });
     }
 }
