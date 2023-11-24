cbee075e9e0dcdaee50ba3ae8eb37e16a7af8cad
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-11-24 18:06:54 -0500

    feat: expertservices added

--- src/services/BaseService-api.service.js
+++ src/services/BaseService-api.service.js
@@ -11,6 +11,16 @@ export class BaseService {
         this.http.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     }
 
+
+    async getAll(endpoint) {
+        try {
+            const response = await this.http.get(`/${endpoint}`);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Error getting users: ${error}`);
+        }
+    }
+
     async getUsersByUsername(endpoint, username) {
         try {
             const response = await this.http.get(`/${endpoint}/getbyusername/${username}`);
