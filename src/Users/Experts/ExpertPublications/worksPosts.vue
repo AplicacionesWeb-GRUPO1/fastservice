f0044ca25097f4836cb8ac114d0a8196b97d717f
Fiorella <U202120418@upc.edu.pe>
2023-11-25 03:08:29 -0500

    feature/works2

--- src/Users/Experts/ExpertPublications/worksPosts.vue
+++ src/Users/Experts/ExpertPublications/worksPosts.vue
@@ -1,43 +1,3 @@
-<template>
-  <div class="expert-home">
-    <!-- Muestra los contratos en estado 'trabajando' del usuario -->
-    <Card v-for="contract in userContracts" :key="contract.id" class="contract-card">
-      <div class="p-fluid">
-        <div class="contract-details">
-          <div class="contract-info">
-            <div class="contract-label">ID del Contrato:</div>
-            <div class="contract-value">{{ contract.id }}</div>
-          </div>
-          <div class="contract-info">
-            <div class="contract-label">Dirección:</div>
-            <div class="contract-value">{{ contract.address }}</div>
-          </div>
-          <div class="contract-info">
-            <div class="contract-label">Título:</div>
-            <div class="contract-value">{{ contract.title }}</div>
-          </div>
-          <div class="contract-info">
-            <div class="contract-label">Descripción:</div>
-            <div class="contract-value">{{ contract.description }}</div>
-          </div>
-          <div class="contract-info">
-            <div class="contract-label">Publicado:</div>
-            <div class="contract-value">{{ contract.isPublished ? 'Sí' : 'No' }}</div>
-          </div>
-          <div class="contract-info">
-            <div class="contract-label">ID del Cliente:</div>
-            <div class="contract-value">{{ contract.clientId }}</div>
-          </div>
-          <div class="contract-info">
-            <div class="contract-label">Imagen:</div>
-            <div class="contract-value">{{ contract.image }}</div>
-          </div>
-        </div>
-      </div>
-    </Card>
-  </div>
-</template>
-
 <script>
 import { ContractServiceApiService } from "@/services/ContractService-api.service";
 
@@ -71,6 +31,90 @@ export default {
 };
 </script>
 
+
+<template>
+  <div class="expert-home">
+    <!-- Muestra los contratos en estado 'trabajando' del usuario -->
+    <Card v-for="contract in userContracts" :key="contract.id" class="contract-card">
+      <div class="p-fluid">
+        <div class="contract-details">
+          <div class="image-info-container">
+            <img :src="contract.publication.image" alt="Imagen de la Publicación" class="publication-image">
+            <div class="contract-info">
+              <div class="contract-label">Publicación:</div>
+              <div class="contract-value">{{ contract.publication.title }}</div>
+              <!-- Agregar más campos según sea necesario -->
+              <div class="contract-label">Dirección:</div>
+              <div class="contract-value">{{ contract.publication.address }}</div>
+              <!-- Agregar más campos según sea necesario para la información de la publicación -->
+              <div class="contract-label">Nombre del Cliente:</div>
+              <div class="contract-value">{{ contract.publication.client.name }}</div>
+              <!-- Agregar más campos según sea necesario para la información del cliente -->
+              <div class="contract-label">Nombre del Experto:</div>
+              <div class="contract-value">{{ contract.expert.name }}</div>
+              <!-- Agregar más campos según sea necesario para la información del experto -->
+              <!-- Información de la fecha, precio, etc. -->
+              <div class="contract-label">Precio:</div>
+              <div class="contract-value">{{ contract.price }}</div>
+              <div class="contract-label">Estado:</div>
+              <div class="contract-value">{{ contract.state }}</div>
+              <div class="contract-label">Fecha:</div>
+              <div class="contract-value">{{ contract.date }}</div>
+              <!-- Puedes seguir agregando más campos según sea necesario -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  </div>
+</template>
+
 <style scoped>
-/* Agrega estilos según sea necesario */
+.expert-home {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.contract-card {
+  width: 100%;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  color: #000000;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.contract-details {
+  display: flex;
+  flex-direction: column;
+}
+
+.image-info-container {
+  display: flex;
+}
+
+.contract-info {
+  margin-left: 10px;
+}
+
+.contract-label {
+  font-weight: bold;
+  color: #000000;
+}
+
+.contract-value {
+  color: #000000;
+}
+
+.publication-image {
+  max-width: 100%;
+  max-height: 200px;
+  margin-bottom: 10px;
+  display: block;
+}
 </style>
+
+
+
