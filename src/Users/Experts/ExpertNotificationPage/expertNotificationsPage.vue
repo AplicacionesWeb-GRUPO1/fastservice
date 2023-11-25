f5b2f460d8cf4a7448f691aad8289dbbcd6d6058
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-11-24 23:26:44 -0500

    feat: ExpertNotification card

--- src/Users/Experts/ExpertNotificationPage/expertNotificationsPage.vue
+++ src/Users/Experts/ExpertNotificationPage/expertNotificationsPage.vue
@@ -1,13 +1,38 @@
 <script>
-import CardComponent from "@/clients/NotificationsPage/Components/clientNotificationCard.vue";
+import ExpertNotificationCard from "@/Users/Experts/ExpertNotificationPage/Components/expertNotificationCard.vue";
+import {ContractServiceApiService} from "@/services/ContractService-api.service";
 
 export default {
   name: 'ExpertNotifications',
-  components: {CardComponent}
+  components: {ExpertNotificationCard},
+  data() {
+    return {
+      notifications: [],
+    };
+  },
+  created() {
+    this.getNotifications();
+  },
+  methods: {
+    async getNotifications() {
+      const currentUser = JSON.parse(localStorage.getItem('user'));
+      const contractServiceApiService = new ContractServiceApiService();
+      try {
+        const contractsNotifications = await contractServiceApiService.getContractsProgress(currentUser.id);
+        this.notifications = contractsNotifications;
+        console.log("asd", contractsNotifications);
+      } catch (error) {
+        console.error("Error fetching job posts:", error);
+      }
+    },
+    eliminarTarjeta(id) {
+      this.notifications = this.notifications.filter((notificacion) => notificacion.id !== id);
+    },
+  },
 }
 </script>
 <div>
-<card-component
+<expert-notification-card
     v-for="notificacion in notifications"
     :key="notificacion.id"
     :nombre="notificacion.expert.name"
